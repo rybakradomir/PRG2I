be3a7627c75ef6abce4fdd7569222903ee5a9427
Radomír Rybák <rybak@odbornaskola.cz>
2023-11-02 10:12:32 +0100

    Initial commit

--- nuzky.py
+++ nuzky.py
@@ -19,3 +19,15 @@ while True:
     elif volby[nahodne_cislo] == "nůžky" and vstup_uzivatele.lower() == "papír":
         bodyPc = bodyPc+1
         pass
+    elif volby[nahodne_cislo] == "papír" and vstup_uzivatele.lower() == "kámen":
+        bodyPc = bodyPc + 1
+        pass
+    elif volby[nahodne_cislo] == "papír" and vstup_uzivatele.lower() == "nůžky":
+        bodyHrac = bodyHrac+1
+        pass
+    elif volby[nahodne_cislo] == "kámen" and vstup_uzivatele.lower() == "papír":
+        bodyHrac = bodyHrac + 1
+        pass
+    elif volby[nahodne_cislo] == "nůžky" and vstup_uzivatele.lower() == "kámen":
+        bodyHrac = bodyHrac + 1
+        pass
